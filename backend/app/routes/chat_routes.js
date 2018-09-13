5d26b6086638eced50b15ebde7cadeeca3272444
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2018-09-13 20:58:45 +0200

    add basic routes

--- backend/app/routes/chat_routes.js
+++ backend/app/routes/chat_routes.js
@@ -1,6 +1,15 @@
 module.exports = function(app, db) {
   app.post('/message', (req, res) => {
     console.log(req.body);
-    res.send(req.body.text);
+    res.send('Nice message!');
+  });
+
+  app.post('/appointment', (req, res) => {
+    console.log(req.body);
+    res.send('Thank you for setting an appointment!');
+  });
+
+  app.get('/greeting', (req, res) => {
+    res.send('When would you like to book an appointment?');
   });
 }
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 8000;
 
+const db = {
+  appointments: [],
+};
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
+require('./app/routes')(app, db);
 app.listen(port, () => {
   console.log('Listening for requests on: ' + port);
 });
